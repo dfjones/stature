1dba6980a3f1a6230cd953ae030adf77d5d61d1a
Doug Jones <doug@dfjones.net>
2012-06-04 23:17:21 -0400

    A little better formatting for some definitions.

--- src/stature/core.clj
+++ src/stature/core.clj
@@ -3,7 +3,7 @@
 (use 'lamina.core 'aleph.udp)
 
 (def messages (ref []))
-(def max-message-count 10)
+(def ^:const max-message-count 10)
 
 (defn print-messages
   [key identity old new]
@@ -12,7 +12,8 @@
 
 (add-watch messages :print-watch print-messages)
 
-(defn append-message [message]
+(defn append-message
+  [message]
   (dosync
     (alter messages
       #(if (>= (count %1) max-message-count)
@@ -21,15 +22,16 @@
       message)))
 
 
-(defn get-message-text [message]
+(defn get-message-text
+  [message]
   (.trim (new String (.array (:message message)))))
 
 (def actions (comp append-message get-message-text))
 
-(defn start-socket []
+(defn start-socket
+  []
   (let [channel @(udp-socket {:port 10000})]
     (map* actions channel)))
 
 (start-socket)
-
 
